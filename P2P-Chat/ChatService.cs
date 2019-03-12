879bbd591b1ea87ba71b59732310b557dfc61fdf
Rafael Paul Weingartner <rweingartner-ortner.its-m2018@fh-salzburg.ac.at>
2019-03-12 19:55:13 +0100

    Added sample implementation.

---
+++ P2P-Chat/ChatService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P2P_Chat
+{
+    public class ChatService
+    {
+        delegate void AddMessage(string message);
+
+        public string userName;
+
+        const int port = 54545;
+        const string broadcastAddress = "255.255.255.255";
+
+        UdpClient receivingClient;
+        UdpClient sendingClient;
+
+        public ChatService(string userName)
+        {
+            this.userName = userName;
+        }
+
+        public void Initialize()
+        {
+            InitSender();
+            InitReceiver();
+        }
+
+        public void SendMessage(string ms)
+        {
+            string toSend = $"[{userName}]: {ms}";
+            byte[] data = Encoding.ASCII.GetBytes(toSend);
+            sendingClient.Send(data, data.Length);
+        }
+
+        private void InitReceiver()
+        {
+            receivingClient = new UdpClient(port);
+
+            ThreadStart start = new ThreadStart(Receiver);
+            var receivingThread = new Thread(start);
+            receivingThread.IsBackground = true;
+            receivingThread.Start();
+        }
+
+        private void InitSender()
+        {
+            sendingClient = new UdpClient(broadcastAddress, port);
+            sendingClient.EnableBroadcast = true;
+        }
+
+        private void Receiver()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, port);
+            AddMessage messageDelegate = MessageReceived;
+
+            while (true)
+            {
+                byte[] data = receivingClient.Receive(ref endPoint);
+                string message = Encoding.ASCII.GetString(data);
+                messageDelegate?.Invoke(message);
+            }
+        }
+
+        private void MessageReceived(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("(room) " + message);
+            Console.ResetColor();
+        }
+    }
+}
--- P2P-Chat/Program.cs
+++ P2P-Chat/Program.cs
@@ -10,6 +10,29 @@ namespace P2P_Chat
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter Username: ");
+            string input = "";
+            while (string.IsNullOrEmpty(input))
+            {
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Welcome " + input);
+            var chatService = new ChatService(input);
+
+            Console.WriteLine("Initializing chat service...");
+            chatService.Initialize();
+
+            Console.WriteLine("Done!\n-----------------------------");
+            input = "";
+            while (!input.Equals("exit"))
+            {
+                //Console.Write(">");
+                input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input))
+                {
+                    chatService.SendMessage(input);
+                }
+            }
         }
     }
 }

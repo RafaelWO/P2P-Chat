f9b543181ce3d5f3b06d0a8bbf083131b7111d81
Rafael Weingartner-Ortner <weingartner.rafael@hotmail.com>
2019-03-19 11:37:09 +0100

    Added project Cryptography test.

---
+++ CryptographyTest/Throw.cs
@@ -0,0 +1,14 @@
+﻿namespace CryptographyTest
+{
+    public class Throw
+    {
+        public Throw(int value1, int value2)
+        {
+            Value = (value1 >= value2) 
+                ? value1 * 10 + value2 
+                : value2 * 10 + value1;
+        }
+
+        public int Value { get; set; }
+    }
+}
